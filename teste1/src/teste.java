a38957341bd98860240f4d7b46f24d8e3cc65759
Cauan.KMS <cauanv@hotmail.com>
2017-04-01 18:35:45 -0300

    Teste1
    Making it more visible

---
+++ teste1/src/teste.java
@@ -0,0 +1,11 @@
+//Teste nice do Java
+public class teste {
+    
+    public static void main(String[] args ){
+    //Isso vai aparecer na entrada do Hello world
+    String txt = "Hello World"; String txt2 ="\"Hello World\""; String txt3="Hello \nWorld"; 
+    System.out.println("TEXTO 1: \n"+txt);
+    System.out.println("\nTEXTO 2: \n"+txt2);
+    System.out.println("\nTEXTO 3: \n"+txt3);
+    }
+}
---
+++ teste1/src/teste2.java
@@ -0,0 +1,14 @@
+//Teste nice 2
+public class teste2 {
+    
+    public static void main(String[] args){
+    //Comeco do Programa
+    int num = 16;
+    
+    if(num==13){ 
+        System.out.println("O num eh "+num);}
+    else{
+        System.out.println("O num NAO eh 13.");
+    }
+    }
+}
