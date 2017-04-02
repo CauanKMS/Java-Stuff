318777d603f5c737786df00b497153a59b4e8f1f
Cauan.KMS <cauanv@hotmail.com>
2017-04-02 18:52:09 -0300

    Random
    Just a random program... No, kidding, it's a program to generate random
    numbers.

---
+++ Random/src/randomClass.java
@@ -0,0 +1,7 @@
+import java.util.Random; //import do Random
+public class randomClass {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(100)); //r.nextInt([max number])
+    }
+}
